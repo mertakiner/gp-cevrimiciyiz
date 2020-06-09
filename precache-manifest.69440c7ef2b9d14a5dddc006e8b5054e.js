5c1d07d7be49374744d657f5ea9270e998e6116d
mert <mac@Merts-iMac.local>
2020-06-09 15:37:34 +0300

    Updates

---
+++ precache-manifest.69440c7ef2b9d14a5dddc006e8b5054e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3760466f7e7930f8240041f2ec2d9dee",
+    "url": "/gp-cevrimiciyiz/index.html"
+  },
+  {
+    "revision": "da8b8a66eaa5255982ee",
+    "url": "/gp-cevrimiciyiz/static/js/2.a06342e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gp-cevrimiciyiz/static/js/2.a06342e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b62d510463a95242de4",
+    "url": "/gp-cevrimiciyiz/static/js/main.066e67d3.chunk.js"
+  },
+  {
+    "revision": "ef5870918e88252a9880",
+    "url": "/gp-cevrimiciyiz/static/js/runtime-main.4934dac7.js"
+  }
+]);
